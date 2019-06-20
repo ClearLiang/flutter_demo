1863c376f919f944e334ae640049c44617a1baa9
ClearLiang <1115624432@qq.com>
2019-06-20 13:58:35 +0800

    添加修改后的dio

---
+++ lib/entity_factory.dart
@@ -0,0 +1,16 @@
+import 'package:vp_flutter/beans/movie_entity.dart';
+import 'package:vp_flutter/beans/user1_entity.dart';
+
+class EntityFactory {
+  static T generateOBJ<T>(json) {
+    if (1 == 0) {
+      return null;
+    } else if (T.toString() == "MovieEntity") {
+      return MovieEntity.fromJson(json) as T;
+    } else if (T.toString() == "User1Entity") {
+      return User1Entity.fromJson(json) as T;
+    } else {
+      return null;
+    }
+  }
+}
